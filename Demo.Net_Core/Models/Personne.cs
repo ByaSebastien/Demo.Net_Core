7f4439c92bbb0a73c48d13e7ee3af14513962f07
Sebastien <byasebastien@hotmail.com>
2023-04-03 13:49:00 +0200

    Add project files.

---
+++ Demo.Net_Core/Models/Personne.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Net_Core.Models
+{
+    public enum Genres
+    {
+        Homme,
+        Femme,
+        Autre
+    }
+    public class Personne
+    { 
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Date { get; set; }
+        public Genres Genre { get; set; }
+    }
+}
---
+++ Demo.Net_Core/Program.cs
@@ -0,0 +1,15 @@
+﻿using Demo.Net_Core.Models;
+
+Console.WriteLine("Hello, World!");
+Personne p = new();
+Personne personne = new Personne()
+{
+    FirstName = "Seb",
+    LastName = "Bya",
+    Date = new DateTime(1991, 3, 27),
+    Genre = Genres.Homme
+};
+personne.FirstName = "Seb";
+personne.LastName = "Bya";
+personne.Date = new DateTime(1991, 3, 27);
+personne.Genre = Genres.Homme;
